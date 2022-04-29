02ec36d8a85a972a09f45fb1208a6a85e56d1219
XYGDeveloper <15243228311@163.com>
2022-04-29 09:14:28 +0800

    - add swift package Manager

---
+++ Sources/CustomClenderPicker.xcframework/ios-arm64_x86_64-maccatalyst/CustomClenderPicker.framework/Versions/A/Headers/CustomClenderPicker.h
@@ -0,0 +1,18 @@
+//
+//  CustomClenderPicker.h
+//  CustomClenderPicker
+//
+//  Created by xyg on 2022/4/28.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomClenderPicker.
+FOUNDATION_EXPORT double CustomClenderPickerVersionNumber;
+
+//! Project version string for CustomClenderPicker.
+FOUNDATION_EXPORT const unsigned char CustomClenderPickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomClenderPicker/PublicHeader.h>
+
+
